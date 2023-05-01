7d99003fa366b762aa7651064c94a037b8bcae0b
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-05-01 01:15:56 -0300

    added categories on Home Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-/*eslint-disable react/prop-types*/
+/*eslint-disable react/react-in-jsx-scope*/
 import {FaShoppingCart} from 'react-icons/fa';
 import {BsPersonCircle} from 'react-icons/bs';
 import styled from 'styled-components';
